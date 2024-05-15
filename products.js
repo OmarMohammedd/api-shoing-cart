a36ed6e2c48eb21cd83236d9bcf412809ad2204f
Omar Mohamed <99252081+OmarMohammedd@users.noreply.github.com>
2024-05-16 00:04:38 +0300

    frist

---
+++ products.js
@@ -0,0 +1,88 @@
+const products = [
+  {
+    id: 1,
+    productName: "ُEl Maleka",
+    price: 17,
+    imageLink: "https://res.cloudinary.com/dglafz8eh/image/upload/v1715805354/download_ftomrh.jpg",
+  },
+  {
+    id: 2,
+    productName: "Andomy",
+    price: 15,
+    imageLink: "https://res.cloudinary.com/dglafz8eh/image/upload/v1715805354/downloassaad_weoill.jpg",
+  },
+  {
+    id: 3,
+    productName: "Italiano",
+    price: 20,
+    imageLink: "https://res.cloudinary.com/dglafz8eh/image/upload/v1715805355/clvdhkwev06rw01hr6jpxbt2w_0_Photoroom-_D9_A2_D9_A0_D9_A2_D9_A4_D9_A0_D9_A4_D9_A2_D9_A4__D9_A0_D9_A9_D9_A1_D9_A6_D9_A5_D9_A6_mwfbyq.jpg",
+  },
+  {
+    id: 4,
+    productName: "Brek",
+    price: 250,
+    imageLink: "https://res.cloudinary.com/dglafz8eh/image/upload/v1715805356/download_1_xce5mw.jpg",
+  },
+  {
+    id: 5,
+    productName: "Big chips cheese",
+    price: 10,
+    imageLink: "https://res.cloudinary.com/dglafz8eh/image/upload/v1715805359/BQ3IfADbH2BhYmXXDdYh6rBynDoarpNZsH03NxYT_ndktl2.webp",
+  },
+  {
+    id: 6,
+    productName: "Big chips, vinegar and salt",
+    price: 10,
+    imageLink: "https://res.cloudinary.com/dglafz8eh/image/upload/v1715805355/CJeTqjcQgDoeSPDltd6QvNFutduue5jQG7fpDyg2_sxjizu.webp",
+  },
+  {
+    id: 7,
+    productName: "Big Cola",
+    price: 12,
+    imageLink: "https://res.cloudinary.com/dglafz8eh/image/upload/v1715805354/Big-Cola-Carbonated-Soft-Drink-200-ML-1-768x768_kxl37b.webp",
+  },
+  {
+    id: 8,
+    productName: "Toren Wafers",
+    price: 25,
+    imageLink: "https://res.cloudinary.com/dglafz8eh/image/upload/v1715805709/download_2_go16j1.jpg",
+  },
+  {
+    id: 9,
+    productName: "Tuna",
+    price: 25,
+    imageLink: "https://res.cloudinary.com/dglafz8eh/image/upload/v1715805356/images_c5fwbu.jpg",
+  },
+  {
+    id: 10,
+    productName: "Suntop juice",
+    price: 10,
+    imageLink: "https://res.cloudinary.com/dglafz8eh/image/upload/v1715805355/images_1_yphjum.jpg",
+  },
+  {
+    id: 11,
+    productName: "Bean",
+    price: 70,
+    imageLink: "https://res.cloudinary.com/dglafz8eh/image/upload/v1715805354/684500705926_peqc2f.webp",
+  },
+  {
+    id: 12,
+    productName: "Knorr Noodles",
+    price: 10,
+    imageLink: "https://res.cloudinary.com/dglafz8eh/image/upload/v1715805359/Knorr-Instant-Noodles-Vegetables-71-g-1-768x768_zlzke9.webp",
+  },
+  {
+    id: 13,
+    productName: "Corona",
+    price: 17.50,
+    imageLink: "https://res.cloudinary.com/dglafz8eh/image/upload/v1715805356/images_2_kgh74q.jpg",
+  },
+  {
+    id: 14,
+    productName: "V7",
+    price: 15,
+    imageLink: "https://res.cloudinary.com/dglafz8eh/image/upload/v1715805356/images_3_p1cioe.jpg",
+  },
+ ];
+ module.exports = products;
+ 
